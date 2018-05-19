5e7eea87e55c7d1d70e9674693408d3296f3bf44
Tatiana Melnychuk <afromyak13@gmail.com>
2018-05-19 18:53:00 +0300

    6.1

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve();
+        }, seconds * 1000);
+    });
 }
 
 /*
@@ -25,6 +30,32 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.send();
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 400) {
+                reject('Загрузка не удалась');
+            } else {
+                let Towns = JSON.parse(xhr.responseText);
+
+                Towns.sort(( a, b ) => {
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+
+                    return 0;
+                });
+                resolve(Towns);
+            }
+        })
+    })
 }
 
 export {
